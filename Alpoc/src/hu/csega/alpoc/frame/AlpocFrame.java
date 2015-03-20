d31d4de86286e8b9238d039d871611922879245e
pcsengodi <petercsengodi@users.noreply.github.com>
2015-03-20 17:46:27 +0100

    project init

---
+++ Alpoc/src/hu/csega/alpoc/frame/AlpocFrame.java
@@ -0,0 +1,18 @@
+package hu.csega.alpoc.frame;
+
+import javax.swing.JFrame;
+
+public class AlpocFrame extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+
+	public AlpocFrame() {
+		super("A little piece of crap");
+	}
+	
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+}
