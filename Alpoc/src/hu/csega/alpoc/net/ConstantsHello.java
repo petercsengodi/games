a9daa1a960607b9a7532fc24033cf7b72d07dc09
pcsengodi <petercsengodi@users.noreply.github.com>
2016-01-31 17:26:25 +0100

    udp comm start

---
+++ Alpoc/src/hu/csega/alpoc/net/ConstantsHello.java
@@ -0,0 +1,9 @@
+package hu.csega.alpoc.net;
+
+import java.nio.charset.Charset;
+
+public class ConstantsHello {
+
+	public static final int PORT = 1080;
+	public static final Charset CHARSET = Charset.forName("UTF-8");
+}
---
+++ Alpoc/src/hu/csega/alpoc/net/SayHello.java
@@ -0,0 +1,37 @@
+package hu.csega.alpoc.net;
+
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
+public class SayHello {
+
+	public static void main(String[] args) throws Exception {
+	    String hostname= "localhost";
+	    int port=ConstantsHello.PORT;
+	    InetAddress host;
+	    DatagramSocket socket;
+	    DatagramPacket packet;	
+	    
+	    
+        try
+        {
+            host = InetAddress.getByName(hostname);
+            socket = new DatagramSocket (null);
+            byte[] bytes = "hello".getBytes(ConstantsHello.CHARSET);
+			packet=new DatagramPacket (bytes, 0,host, port);
+            socket.send (packet);
+            // packet.setLength(100);
+            // socket.receive (packet);
+            socket.close ();
+//            byte[] data = packet.getData ();
+//            String time=new String(data);  // convert byte array data into string
+//            System.out.println(time);
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+        }
+	}
+
+}
