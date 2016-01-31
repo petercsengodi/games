ffafa69f9a40f4100d650ab847eccd7dc6a5cfc2
Peter Csengodi <pcsengodi@skawa.hu>
2016-01-31 18:35:44 +0100

    send data broadcast

---
+++ Alpoc/src/hu/csega/alpoc/net/ReceiveHelloBr.java
@@ -0,0 +1,41 @@
+package hu.csega.alpoc.net;
+
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
+public class ReceiveHelloBr {
+
+	public static void main(String[] args) throws Exception {
+		// Create a socket to listen on the port.
+		try (DatagramSocket socket = new DatagramSocket(ConstantsHello.PORT,
+				InetAddress.getByName("0.0.0.0"))) {
+			socket.setBroadcast(true);
+
+			System.out.println(">>>Ready to receive broadcast packets!");
+			byte[] recvBuf = new byte[15000];
+			DatagramPacket packet = new DatagramPacket(recvBuf, recvBuf.length);
+			socket.receive(packet);
+
+			System.out.println(">>>Discovery packet received from: "
+					+ packet.getAddress().getHostAddress());
+			System.out.println(">>>Packet received; data: "
+					+ new String(packet.getData()));
+
+			String message = new String(packet.getData()).trim();
+			if (message.equals("hello")) {
+				byte[] sendData = "ack".getBytes();
+
+				DatagramPacket sendPacket = new DatagramPacket(sendData,
+						sendData.length, packet.getAddress(), packet.getPort());
+				socket.send(sendPacket);
+				System.out.println(">>>Sent packet to: "
+						+ sendPacket.getAddress().getHostAddress());
+			}
+
+		} catch (Exception e) {
+			System.err.println(e);
+		}
+	}
+
+}
---
+++ Alpoc/src/hu/csega/alpoc/net/SayHelloBr.java
@@ -0,0 +1,72 @@
+package hu.csega.alpoc.net;
+
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.InterfaceAddress;
+import java.net.NetworkInterface;
+import java.util.Enumeration;
+
+public class SayHelloBr {
+
+	public static void main(String[] args) throws Exception {
+		byte[] sendData = "hello".getBytes(ConstantsHello.CHARSET);
+
+		try {
+			try (DatagramSocket socket = new DatagramSocket()) {
+				socket.setBroadcast(true);
+				
+				{
+					DatagramPacket sendPacket = new DatagramPacket(sendData,
+							sendData.length,
+							InetAddress.getByName("255.255.255.255"),
+							ConstantsHello.PORT);
+	
+					socket.send(sendPacket);
+					System.out.println(">>> Request packet sent to: 255.255.255.255 (DEFAULT)");
+				}
+
+				Enumeration<NetworkInterface> interfaces = NetworkInterface.getNetworkInterfaces();
+				while (interfaces.hasMoreElements()) {
+					NetworkInterface networkInterface = interfaces.nextElement();
+
+					if (networkInterface.isLoopback() || !networkInterface.isUp()) {
+						continue; // Don't want to broadcast to the loopback interface
+					}
+
+					for (InterfaceAddress interfaceAddress : networkInterface.getInterfaceAddresses()) {
+						InetAddress broadcast = interfaceAddress.getBroadcast();
+						if (broadcast == null) {
+							continue;
+						}
+
+						try {
+							DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, broadcast, 8888);
+							socket.send(sendPacket);
+						} catch (Exception e) {
+						}
+
+						System.out.println(">>> Request packet sent to: "
+								+ broadcast.getHostAddress() + "; Interface: "
+								+ networkInterface.getDisplayName());
+					}
+				}
+
+				System.out.println(">>> Done looping over all network interfaces. Now waiting for a reply!");
+
+				byte[] recvBuf = new byte[15000];
+				DatagramPacket receivePacket = new DatagramPacket(recvBuf, recvBuf.length);
+				socket.receive(receivePacket);
+
+				System.out.println(">>> Broadcast response from server: " + receivePacket.getAddress().getHostAddress());
+
+				String message = new String(receivePacket.getData()).trim();
+				System.out.println(">>> message: " + message);
+			} catch (Exception e) {
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
