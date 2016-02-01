014d5e7ebc440ab2e72012f7b3386bc4a09e23ca
pcsengodi <petercsengodi@users.noreply.github.com>
2016-02-01 21:47:50 +0100

    dynamic class loading with annotations

--- Alpoc/src/hu/csega/dyn/DynamicFramework.java
+++ Alpoc/src/hu/csega/dyn/DynamicFramework.java
@@ -1,6 +1,14 @@
 package hu.csega.dyn;
 
+import java.lang.reflect.AnnotatedElement;
+import java.lang.reflect.Method;
+import java.net.URL;
+import java.net.URLClassLoader;
 import java.nio.charset.Charset;
+import java.util.List;
+import java.util.Set;
+
+import org.scannotation.AnnotationDB;
 
 public class DynamicFramework {
 
@@ -14,4 +22,37 @@ public class DynamicFramework {
 	public static final int TCP_FLOW_PORT = 1082;
 	
 	public static final Charset CHARSET = Charset.forName("UTF-8");
+	
+
+	public static Set<String> search(URL url) throws Exception {
+		AnnotationDB db = new AnnotationDB();
+		db.scanArchives(url);
+		Set<String> entityClasses =
+		    db.getAnnotationIndex().get(Agent.class.getName());
+		return entityClasses;
+	}
+
+	public static void main(String[] args) throws Exception {
+		URL url = new URL("file:///tmp/tmp.jar");
+		Set<String> set = search(url);
+		
+		String firstClass = null;
+		if(set != null) {
+			for(String str : set) {
+				System.out.println("found: " + str);
+				if(firstClass == null)
+					firstClass = str;
+			}
+		}
+		
+		System.out.println("first: " + firstClass);
+		
+	    try (URLClassLoader classLoader = new URLClassLoader(new URL[]{url})) {
+	        Class<?> aClass = classLoader.loadClass(firstClass);
+	        Method method = aClass.getMethod("main", new Class<?>[]{ String[].class });
+	        method.invoke(null, new Object[] { null });
+	    } catch (ClassNotFoundException e) {
+	        e.printStackTrace();
+	    }
+	}
 }
