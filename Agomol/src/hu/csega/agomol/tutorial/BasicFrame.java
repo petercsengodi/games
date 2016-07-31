2935bb9188a4bdade7d3e8d142b0065921c81842
petercsengodi <petercsengodi@users.noreply.github.com>
2016-07-31 14:11:53 +0200

    demos

---
+++ Agomol/src/hu/csega/agomol/tutorial/BasicFrame.java
@@ -0,0 +1,55 @@
+package hu.csega.agomol.tutorial;
+
+import javax.swing.JFrame;
+
+import com.jogamp.opengl.GLAutoDrawable;
+import com.jogamp.opengl.GLCapabilities;
+import com.jogamp.opengl.GLEventListener;
+import com.jogamp.opengl.GLProfile;
+import com.jogamp.opengl.awt.GLCanvas;
+
+public class BasicFrame implements GLEventListener {
+
+	   @Override
+	   public void display(GLAutoDrawable arg0) {
+	      // method body
+	   }
+
+	   @Override
+	   public void dispose(GLAutoDrawable arg0) {
+	      //method body
+	   }
+
+	   @Override
+	   public void init(GLAutoDrawable arg0) {
+	      // method body
+	   }
+
+	   @Override
+	   public void reshape(GLAutoDrawable arg0, int arg1, int arg2, int arg3, int arg4) {
+	      // method body
+	   }
+
+	   public static void main(String[] args) {
+
+	      //getting the capabilities object of GL2 profile
+	      final GLProfile profile = GLProfile.get(GLProfile.GL2);
+	      GLCapabilities capabilities = new GLCapabilities(profile);
+
+	      // The canvas
+	      final GLCanvas glcanvas = new GLCanvas(capabilities);
+	      BasicFrame b = new BasicFrame();
+	      glcanvas.addGLEventListener(b);
+	      glcanvas.setSize(400, 400);
+
+	      //creating frame
+	      final JFrame frame = new JFrame (" Basic Frame");
+
+	      //adding canvas to it
+	      frame.getContentPane().add(glcanvas);
+	      frame.setSize(frame.getContentPane().getPreferredSize());
+	      frame.setVisible(true);
+
+	   }//end of main
+
+	}//end of classimport
