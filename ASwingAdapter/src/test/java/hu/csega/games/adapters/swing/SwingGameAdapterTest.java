d1a53a0f2e1acc3481829bdd8bfc111e755183f6
Peter Csengodi <pcsengodi@skawa.hu>
2017-05-02 13:00:49 +0200

    mavenized some projects

---
+++ ASwingAdapter/src/test/java/hu/csega/games/adapters/swing/SwingGameAdapterTest.java
@@ -0,0 +1,21 @@
+package hu.csega.games.adapters.swing;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class SwingGameAdapterTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+	}
+
+}
