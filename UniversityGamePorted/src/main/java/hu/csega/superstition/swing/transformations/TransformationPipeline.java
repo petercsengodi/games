5ccddf4b1efa213d5de27efe3ac6f0bed72db056
petercsengodi <petercsengodi@users.noreply.github.com>
2017-12-04 07:30:05 +0100

    transformation pipeline

--- UniversityGamePorted/src/main/java/hu/csega/superstition/swing/transformations/TransformationPipeline.java
+++ UniversityGamePorted/src/main/java/hu/csega/superstition/swing/transformations/TransformationPipeline.java
@@ -1,5 +1,31 @@
 package hu.csega.superstition.swing.transformations;
 
-public class TransformationPipeline {
+import java.util.ArrayList;
+import java.util.List;
 
+import hu.csega.games.engine.g2d.GamePoint;
+
+public class TransformationPipeline implements Transformation {
+
+	List<Transformation> transformations = new ArrayList<>();
+
+	@Override
+	public GamePoint fromModelToCanvas(GamePoint source) {
+		GamePoint tmp = source;
+
+		for(Transformation transformation : transformations)
+			tmp = transformation.fromModelToCanvas(tmp);
+
+		return tmp;
+	}
+
+	@Override
+	public GamePoint fromCanvasToModel(GamePoint source) {
+		GamePoint tmp = source;
+
+		for(Transformation transformation : transformations)
+			tmp = transformation.fromCanvasToModel(tmp);
+
+		return tmp;
+	}
 }
