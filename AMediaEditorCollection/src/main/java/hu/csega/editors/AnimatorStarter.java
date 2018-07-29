c4934af675bc03f32660edc5df2360075c9978d0
petercsengodi <petercsengodi@users.noreply.github.com>
2018-07-29 11:46:39 +0200

    started writing Animator tool

---
+++ AMediaEditorCollection/src/main/java/hu/csega/editors/AnimatorStarter.java
@@ -0,0 +1,42 @@
+package hu.csega.editors;
+
+import hu.csega.editors.anm.AnimatorConnector;
+import hu.csega.editors.ftm.FreeTriangleMeshConnector;
+import hu.csega.games.common.ApplicationStarter;
+import hu.csega.games.common.Connector;
+import hu.csega.games.library.TextureLibrary;
+import hu.csega.games.library.util.FileUtil;
+import hu.csega.toolshed.logging.Level;
+import hu.csega.toolshed.logging.Logger;
+import hu.csega.toolshed.logging.LoggerFactory;
+
+/**
+ * Responsible for running the Animator tool.
+ * A single instance in the JVM, accessible by any components, and
+ * links all the components together.
+ * However, not static methods are used, as writing a testable code is prio 1!
+ */
+public class AnimatorStarter {
+
+	public static final String DEFAULT_TEXTURE_FILE = "res/textures/ship2.jpg";
+
+	private static final Level LOGGING_LEVEL = Level.TRACE;
+	private static Logger logger;
+
+	public static FileUtil FILES;
+	public static TextureLibrary TEXTURES;
+
+	public static void main(String[] args) {
+		LoggerFactory.setDefaultLevel(LOGGING_LEVEL);
+		logger = LoggerFactory.createLogger(AnimatorStarter.class);
+		logger.info("Starting tool.");
+
+		FILES = new FileUtil("AMediaEditorCollection");
+		TEXTURES = new TextureLibrary(FILES);
+
+		Connector connector = new AnimatorConnector();
+		ApplicationStarter starter = new ApplicationStarter(connector);
+		starter.start(args);
+	}
+
+}
---
+++ AMediaEditorCollection/src/main/java/hu/csega/editors/anm/AnimatorConnector.java
@@ -0,0 +1,160 @@
+package hu.csega.editors.anm;
+
+import java.awt.Container;
+import java.awt.GridLayout;
+import java.awt.event.KeyListener;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.JFrame;
+import javax.swing.JTabbedPane;
+
+import hu.csega.editors.anm.menu.AnimatorMenu;
+import hu.csega.editors.ftm.menu.FreeTriangleMeshMenu;
+import hu.csega.editors.ftm.view.FreeTriangleMeshTexture;
+import hu.csega.editors.ftm.view.FreeTriangleMeshXYSideView;
+import hu.csega.editors.ftm.view.FreeTriangleMeshXZSideView;
+import hu.csega.editors.ftm.view.FreeTriangleMeshZYSideView;
+import hu.csega.games.adapters.opengl.OpenGLGameAdapter;
+import hu.csega.games.common.Connector;
+import hu.csega.games.engine.GameEngineFacade;
+import hu.csega.games.engine.env.Environment;
+import hu.csega.games.engine.impl.GameEngine;
+import hu.csega.games.engine.intf.GameAdapter;
+import hu.csega.games.engine.intf.GameCanvas;
+import hu.csega.games.engine.intf.GameDescriptor;
+import hu.csega.games.engine.intf.GameEngineStep;
+import hu.csega.games.engine.intf.GameWindow;
+import hu.csega.games.engine.intf.GameWindowListener;
+import hu.csega.toolshed.logging.Logger;
+import hu.csega.toolshed.logging.LoggerFactory;
+
+public class AnimatorConnector implements Connector, GameWindow {
+
+	private GameWindow gameWindow;
+	private List<GameWindowListener> listeners = new ArrayList<>();
+
+	@Override
+	public void initialize() {
+	}
+
+	@Override
+	public void run(Environment env) {
+		logger.info(className() + " start run()");
+
+		startGameEngine();
+
+		logger.info(className() + " end run()");
+	}
+
+	@Override
+	public void register(GameWindowListener listener) {
+		listeners.add(listener);
+	}
+
+	@Override
+	public void add(GameCanvas canvas) {
+	}
+
+	@Override
+	public void setFullScreen(boolean fullScreen) {
+	}
+
+	@Override
+	public void showWindow() {
+	}
+
+	@Override
+	public void closeWindow() {
+		for(GameWindowListener listener: listeners) {
+			listener.onFinishingWork();
+		}
+	}
+
+	@Override
+	public void closeApplication() {
+		for(GameWindowListener listener: listeners) {
+			listener.onFinishingWork();
+		}
+		System.exit(0);
+	}
+
+	@Override
+	public void dispose() {
+		logger.info(className() + " start dispose()");
+
+		logger.info(className() + " end dispose()");
+	}
+
+	private GameEngine startGameEngine() {
+
+		GameDescriptor descriptor = new GameDescriptor();
+		descriptor.setId("anm");
+		descriptor.setTitle("Animator Tool");
+		descriptor.setVersion("v00.00.0001");
+		descriptor.setDescription("A tool for creating small character animations.");
+		descriptor.setMouseCentered(false);
+
+		// Open GL View
+		
+		GameAdapter adapter = new OpenGLGameAdapter();
+		GameEngine engine = GameEngine.create(descriptor, adapter);
+		GameEngineFacade facade = engine.getFacade();
+
+		/*
+		engine.step(GameEngineStep.INIT, new FreeTriangleMeshInitStep());
+		engine.step(GameEngineStep.MODIFY, new FreeTriangleMeshModifyStep());
+		engine.step(GameEngineStep.RENDER, new FreeTriangleMeshRenderStep());
+
+		engine.getControl().registerKeyListener(new FreeTriangleMeshKeyListener());
+		 */
+		
+		// Swing View(s)
+		
+		gameWindow = adapter.createWindow(engine);
+		gameWindow.setFullScreen(true);
+
+		JFrame frame = (JFrame) gameWindow;
+		KeyListener keyListener = (KeyListener) gameWindow;
+		Container contentPane = frame.getContentPane();
+		contentPane.setLayout(new GridLayout(2, 2));
+
+		AnimatorMenu.createMenuForJFrame(frame, facade);
+
+		// Upper left tile
+		/*
+		contentPane.add(new FreeTriangleMeshXZSideView(facade));
+		 */
+		
+		// Upper right tile
+		engine.startIn(gameWindow);
+
+		// Lower left tile
+		/*
+		contentPane.add(new FreeTriangleMeshXYSideView(facade));
+		*/
+
+		// Lower right tile
+		/*
+		JTabbedPane bottomRightTab = new JTabbedPane();
+		bottomRightTab.addKeyListener(keyListener);
+		bottomRightTab.addTab("ZY Wireframe", new FreeTriangleMeshZYSideView(facade));
+		bottomRightTab.addTab("Texture Window", new FreeTriangleMeshTexture(facade));
+		contentPane.add(bottomRightTab);
+		 */
+		
+		return engine;
+	}
+
+	private String className() {
+		return getClass().getSimpleName();
+	}
+
+	@Override
+	public void repaintEverything() {
+		if(gameWindow != null)
+			gameWindow.repaintEverything();
+	}
+
+	private static final Logger logger = LoggerFactory.createLogger(AnimatorConnector.class);
+}
