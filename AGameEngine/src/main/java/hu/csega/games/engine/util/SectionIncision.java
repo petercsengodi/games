8d0f371c488e798e80bd793f0f14bb503bb3029f
petercsengodi <petercsengodi@users.noreply.github.com>
2018-08-20 15:46:48 +0200

    fix and new incision check algorithms

--- AGameEngine/src/main/java/hu/csega/games/engine/util/SectionIncision.java
+++ AGameEngine/src/main/java/hu/csega/games/engine/util/SectionIncision.java
@@ -12,7 +12,7 @@ public class SectionIncision {
 	 * This method is only good for checking if there is an incision; it doesn't calculate,
 	 * where exactly the incision is.
 	 */
-	public static boolean incision(
+	public static boolean incisionSections(
 			double x0, double y0, double x1, double y1,
 			double x2, double y2, double x3, double y3) {
 
@@ -20,14 +20,69 @@ public class SectionIncision {
 		double dy2 = y3-y2;
 
 		double B = dx2*(y1-y0) - (x1-x0)*dy2;
-		if(B <= 0) // This eliminates the parallel case.
+		if(Math.abs(B) < EPSILON) // This eliminates the parallel case.
 			return false;
 
 		double A = (x0-x2)*dy2 - dx2*(y0-y2);
-		if(A < 0)
-			return false;
-
-		return A <= B;
+		if(B > 0)
+			return (0 <= A) && (A <= B);
+		else
+			return (0 >= A) && (A >= B);
 	}
 
+	public static boolean incisionSectionAndSphere(
+			double x0, double y0, double x1, double y1,
+			double sx, double sy, double sr) {
+		double dx = x1 - x0;
+		double dy = y1 - y0;
+		double dsx = x0 - sx;
+		double dsy = y0 - sy;
+
+		double a = dx*dx + dy*dy;
+		double b = 2.0*(dsx*dx + dsy*dy);
+		double c = dsx*dsx + dsy*dsy - sr*sr;
+
+		double D = b*b - 4.0*a*c;
+
+		if(Math.abs(D) < EPSILON)
+			return -b <= 2.0*a;
+
+		if(D < 0)
+			return false;
+
+		double rightSide = b+2*a;
+		double rightSide2 = rightSide * rightSide;
+
+		boolean ret = (rightSide < 0 && D >= rightSide2) || (rightSide > 0 && D <= rightSide2);
+		return ret;
+	}
+
+	public static boolean incisionSpheres(
+			double x0, double y0, double r0,
+			double x1, double y1, double r1) {
+		if(Math.abs(x1 - x0) < EPSILON && Math.abs(y1 - y0) < EPSILON)
+			return false;
+
+		double r02 = r0*r0;
+		double r12 = r1*r1;
+		double x02 = x0*x0;
+		double x12 = x1*x1;
+		double y02 = y0*y0;
+		double y12 = y1*y1;
+
+		double dx = x1 - x0;
+		double dy = y1 - y0;
+		double dx2 = dx*dx;
+		double dy2 = dy*dy;
+
+		double C = (r02 - r12 + x12 - x02 + y12 - y02) / 2.0;
+		double E = dy*y0 - C;
+
+		double a = dx2 + dy2;
+		double b = 2.0*(dx*E - dy2*x0);
+		double c = E*E + dy2*(x02 - r02);
+
+		double D = b*b - 4.0*a*c;
+		return D >= 0;
+	}
 }
--- AGameEngine/src/test/java/hu/csega/games/engine/util/SectionIncisionTest.java
+++ AGameEngine/src/test/java/hu/csega/games/engine/util/SectionIncisionTest.java
@@ -1,6 +1,8 @@
 package hu.csega.games.engine.util;
 
-import static hu.csega.games.engine.util.SectionIncision.incision;
+import static hu.csega.games.engine.util.SectionIncision.incisionSections;
+import static hu.csega.games.engine.util.SectionIncision.incisionSectionAndSphere;
+import static hu.csega.games.engine.util.SectionIncision.incisionSpheres;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
@@ -8,21 +10,38 @@ import org.junit.Test;
 public class SectionIncisionTest {
 
 	@Test
-	public void test() {
+	public void testSections() {
 		// Normal incision.
-		assertTrue(incision(12, 5, 12, 10, 0, 6, 20, 6));
-		assertTrue(incision(0, 0, 10, 10, 0, 4, 10, 9));
-		assertFalse(incision(0, 0, 10, 10, 0, 14, 10, 11));
-		assertFalse(incision(0, 0, 10, 10, 0, 14, 12, 9.8));
+		assertTrue(incisionSections(12, 5, 12, 10, 0, 6, 20, 6));
+		assertTrue(incisionSections(0, 0, 10, 10, 0, 4, 10, 9));
+		assertFalse(incisionSections(0, 0, 10, 10, 0, 14, 10, 11));
+		assertFalse(incisionSections(0, 0, 10, 10, 0, 14, 12, 9.8));
+
+		assertTrue(incisionSections(12, 5, 12, 10, 10, 6, -10, 6));
 
 		// Parallel cases.
-		assertFalse(incision(12, 5, 12, 10, 12, 3, 12, 8));
-		assertFalse(incision(12, 5, 12, 7, 12, 8, 12, 13));
-		assertFalse(incision(12, 5, 12, 7, 12, 12, 12, 8));
-		assertFalse(incision(12, 5, 12, 10, 10, 3, 10, 8));
-		assertFalse(incision(0, 10, 4, 10, 3, 10, 16, 10));
-		assertFalse(incision(0, 0, 10, 6, 0, 0, 20, 12));
-		assertFalse(incision(0, 0, 10, 6, 0, 0-1, 20, 12-1));
+		assertFalse(incisionSections(12, 5, 12, 10, 12, 3, 12, 8));
+		assertFalse(incisionSections(12, 5, 12, 7, 12, 8, 12, 13));
+		assertFalse(incisionSections(12, 5, 12, 7, 12, 12, 12, 8));
+		assertFalse(incisionSections(12, 5, 12, 10, 10, 3, 10, 8));
+		assertFalse(incisionSections(0, 10, 4, 10, 3, 10, 16, 10));
+		assertFalse(incisionSections(0, 0, 10, 6, 0, 0, 20, 12));
+		assertFalse(incisionSections(0, 0, 10, 6, 0, 0-1, 20, 12-1));
+	}
+
+	@Test
+	public void testSectionAndShpere() {
+		assertFalse(incisionSectionAndSphere(-10, 22, 10, 22, 0, 0, 20));
+		assertTrue(incisionSectionAndSphere(-10, 20, 10, 20, 0, 0, 20));
+		assertTrue(incisionSectionAndSphere(10, 18, -10, 18, 0, 0, 20));
+	}
+
+	@Test
+	public void testAndShperes() {
+		assertFalse(incisionSpheres(2, 2, 10, 2, 2, 20));
+		assertTrue(incisionSpheres(0, 0, 10, 2, 2, 10));
+		assertTrue(incisionSpheres(0, 0, 20, 20, 20, 20));
+		assertFalse(incisionSpheres(0, 0, 20, 40, 40, 20));
 	}
 
 }
